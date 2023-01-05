9ec3a341e614a06a905cc5d4d2e476c59bcf2c16
chandraauliatama <chandraaulia4@gmail.com>
2023-01-05 18:48:48 +0700

    add invoice model and migration

---
+++ database/migrations/2023_01_05_113821_create_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('worker_name');
+            $table->string('worker_email');
+            $table->string('worker_phone');
+            $table->string('bill_to');
+            $table->string('bill_address');
+            $table->date('invoice_date');
+            $table->string('payment_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
