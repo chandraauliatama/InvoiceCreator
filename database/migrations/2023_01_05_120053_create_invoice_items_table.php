cecaa3a363a2dbc0d66750e666ad227fa652ee6e
chandraauliatama <chandraaulia4@gmail.com>
2023-01-05 19:06:09 +0700

    create invoice items model and migration

---
+++ database/migrations/2023_01_05_120053_create_invoice_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Filament\Forms\Components\Tabs\Tab;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('work_description');
+            $table->foreignId('invoice_id')->constrained()->cascadeOnDelete();
+            $table->date('start_date');
+            $table->date('complete_date');
+            $table->integer('days_worked');
+            $table->integer('pay_per_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+};
